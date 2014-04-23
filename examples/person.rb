275ca79cf5646d2837ec058e61aa1bd6ddbda86d
weshays <weshays@gbdev.com>
2014-04-23 12:29:33 -0700

    Updated person example

--- examples/person.rb
+++ examples/person.rb
@@ -12,26 +12,28 @@ require File.dirname(__FILE__) + '/shelby_arena_keys.rb'
 
 shelby_session = ShelbyArena::Api.request_session(ShelbyArenaKeys::USERNAME, 
                                                   ShelbyArenaKeys::PASSWORD, 
-                                                  ShelbyArenaKeys::API_KEY, 
+                                                  ShelbyArenaKeys::REGULAR_API_KEY, 
                                                   ShelbyArenaKeys::SHELBY_ARENA_SERVICE_URL)
 
-
 ShelbyArena::Api.connect(shelby_session.id, 
-                         ShelbyArenaKeys::API_SECRET, 
+                         ShelbyArenaKeys::REGULAR_API_SECRET, 
                          ShelbyArenaKeys::SHELBY_ARENA_SERVICE_URL)
 
 person_list = ShelbyArena::PersonList.new({campus_id: 3})
 person = person_list[0]
 
 debugger
-asdf=234
+asdf=111
 
 # puts '*****************'
 # puts person_list.inspect
 # puts '*****************'
 # puts '*****************'
 
-# person = ShelbyArena::Person.load_by_id(person.person_id)
+# person2 = ShelbyArena::Person.load_by_id(55144)
+
+# debugger
+# asdf=222
 
 # puts '*****************'
 # puts person.inspect
